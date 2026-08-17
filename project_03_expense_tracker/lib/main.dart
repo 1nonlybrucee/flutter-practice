2f7dfaa452e00cd9ba71dc159a6a250d8a40d76d
Bruce Goyena <goyenabruce@gmail.com>
2026-08-17 20:21:17 +0800

    feat: add expense amounts and category totals

--- project_03_expense_tracker/lib/main.dart
+++ project_03_expense_tracker/lib/main.dart
@@ -8,7 +8,8 @@ class Expense {
   String id;
   Category category;
   String name;
-  Expense(this.id, this.category, this.name);
+  double amount;
+  Expense(this.id, this.category, this.name, this.amount);
 }
 
 class Category {
@@ -21,20 +22,37 @@ class Category {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  double getCategoryTotal(Category category, List<Expense> expenses) {
+    return expenses
+        .where((expense) => expense.category.id == category.id)
+        .fold(0, (sum, expense) => sum + expense.amount);
+  }
+
   @override
   Widget build(BuildContext context) {
-    double totalSpent = 0.00;
     Category category1 = Category("1", "Food");
     Category category2 = Category("2", "Transport");
     Category category3 = Category("3", "School");
 
     List<Category> categories = [category1, category2, category3];
 
-    Expense mockExpense1 = Expense("1", category1, "lunch");
-    Expense mockExpense2 = Expense("2", category2, "fare");
-    Expense mockExpense3 = Expense("3", category3, "project");
+    Expense mockExpense1 = Expense("1", category1, "Lunch", 60.52);
+    Expense mockExpense2 = Expense("2", category2, "Fare", 120.334);
+    Expense mockExpense3 = Expense("3", category3, "Project", 130);
+    Expense mockExpense4 = Expense("4", category1, "Snack", 45);
 
-    List<Expense> expenses = [mockExpense1, mockExpense2, mockExpense3];
+    List<Expense> expenses = [
+      mockExpense1,
+      mockExpense2,
+      mockExpense3,
+      mockExpense4,
+    ];
+    double totalSpent = expenses.fold(
+      0,
+      (sum, expense) => sum + expense.amount,
+    );
+
+    String formattedPrice = totalSpent.toStringAsFixed(2);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -44,7 +62,10 @@ class MyApp extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Text("Total Spent \n₱$totalSpent"),
+              ListTile(
+                title: Text("Total Spent:"),
+                trailing: Text("₱$formattedPrice"),
+              ),
 
               SizedBox(
                 height: 100,
@@ -56,7 +77,12 @@ class MyApp extends StatelessWidget {
                     return SizedBox(
                       width: 115,
                       child: Card(
-                        child: Center(child: Text(categories[index].name)),
+                        child: ListTile(
+                          title: Text(categories[index].name),
+                          subtitle: Text(
+                            "₱${getCategoryTotal(categories[index], expenses).toStringAsFixed(2)}",
+                          ),
+                        ),
                       ),
                     );
                   },
@@ -68,7 +94,15 @@ class MyApp extends StatelessWidget {
                 child: ListView.builder(
                   itemCount: expenses.length,
                   itemBuilder: (context, index) {
-                    return Card(child: Text(expenses[index].name));
+                    return Card(
+                      child: ListTile(
+                        title: Text(expenses[index].name),
+                        subtitle: Text(expenses[index].category.name),
+                        trailing: Text(
+                          "₱${expenses[index].amount.toStringAsFixed(2)}",
+                        ),
+                      ),
+                    );
                   },
                 ),
               ),
