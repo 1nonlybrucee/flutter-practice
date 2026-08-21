4ae02f8a9614354248b7070c194f85a4b94b6e0a
Bruce Goyena <goyenabruce@gmail.com>
2026-08-21 22:09:56 +0800

    feat: implement bottom navigation with home, expenses, and stats screens

--- project_04_expense_tracker_v2/lib/main.dart
+++ project_04_expense_tracker_v2/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'package:project_04_expense_tracker_v2/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: HomePage())),
+      home: Scaffold(body: HomePage()),
     );
   }
 }
--- project_04_expense_tracker_v2/lib/pages/home_page.dart
+++ project_04_expense_tracker_v2/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:project_04_expense_tracker_v2/pages/expenses_screen.dart';
+import 'package:project_04_expense_tracker_v2/pages/home_screen.dart';
+import 'package:project_04_expense_tracker_v2/pages/stats_screen.dart';
+import '/navigation/navigation.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,32 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentIndex = 0;
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeScreen(),
+    const ExpensesScreen(),
+    const StatsScreen(),
+  ];
+
+  void _onNavTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Expense Tracker")),
-
-      body: const Center(child: Text("Home Page")),
-
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: "Expenses"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart),
-            label: "Statistics",
-          ),
-        ],
+      appBar: AppBar(title: Text("Expense Tracker")),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Navigation(
+        currentIndex: _selectedIndex,
+        onTap: _onNavTapped,
       ),
     );
   }
