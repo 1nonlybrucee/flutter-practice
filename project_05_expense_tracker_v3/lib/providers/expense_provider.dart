d3127c09d405a579b2ba5e898982a5054f0523d4
Bruce Goyena <goyenabruce@gmail.com>
2026-08-25 20:10:26 +0800

    added top three categories provider

--- project_05_expense_tracker_v3/lib/providers/expense_provider.dart
+++ project_05_expense_tracker_v3/lib/providers/expense_provider.dart
@@ -11,10 +11,24 @@ class ExpenseNotifier extends Notifier<List<Expense>> {
     date: DateTime.parse(hardcodedString),
     category: Category.kakanon,
   );
+  Expense expense2 = Expense(
+    id: '123',
+    name: "commute psu",
+    amount: 100,
+    date: DateTime.parse(hardcodedString),
+    category: Category.pamasahe,
+  );
+  Expense expense3 = Expense(
+    id: '123',
+    name: "parcel pahamot",
+    amount: 530,
+    date: DateTime.parse(hardcodedString),
+    category: Category.shopee,
+  );
 
   @override
   List<Expense> build() {
-    return [expense1];
+    return [expense1, expense2, expense3];
   }
 
   void addExpense(Expense expense) {
@@ -33,4 +47,23 @@ final expenseProvider = NotifierProvider<ExpenseNotifier, List<Expense>>(
 final totalExpenseProvider = Provider<double>((ref) {
   final expenses = ref.watch(expenseProvider);
   return expenses.fold(0.0, (sum, expense) => sum + expense.amount);
+});
+
+final topThreeCategoriesProvider = Provider<List<MapEntry<Category, double>>>((
+  ref,
+) {
+  final expenses = ref.watch(expenseProvider);
+
+  final categoryTotals = <Category, double>{};
+
+  for (final expense in expenses) {
+    categoryTotals[expense.category] =
+        (categoryTotals[expense.category] ?? 0) + expense.amount;
+  }
+
+  final sortedCategories = categoryTotals.entries.toList();
+
+  sortedCategories.sort((a, b) => b.value.compareTo(a.value));
+
+  return sortedCategories.take(3).toList();
 });
