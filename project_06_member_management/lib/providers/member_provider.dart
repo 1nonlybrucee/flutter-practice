3240599761549bd1d0a60d376845aead08797df8
Bruce Goyena <goyenabruce@gmail.com>
2026-09-03 23:12:18 +0800

    added initial provider

---
+++ project_06_member_management/lib/providers/member_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_06_member_management/models/member.dart';
+
+class MemberNotifier extends Notifier<List<Member>> {
+  @override
+  List<Member> build() {
+    return [];
+  }
+
+  void addMember(Member member) {
+    state = [...state, member];
+  }
+
+  void deleteMember(String idToRemove) {
+    state = state.where((member) => member.id != idToRemove).toList();
+  }
+}
